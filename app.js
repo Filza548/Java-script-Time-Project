3f4d93ebc8e5795ef59ccdff106dfe3e69ab71f6
Filza548 <filzajameel458@gmail.com>
2025-05-23 03:01:46 +0500

    Alhumdulillah done

---
+++ app.js
@@ -0,0 +1,33 @@
+const video = document.getElementById("bg-video");
+    video.playbackRate = 0.7; // 0.5 = half speed (slow)
+
+const hourEl = document.getElementById("hour")
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds")
+const ampmEl = document.getElementById("ampm")
+
+function clocktime(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM"
+
+
+if (h > 12){
+    h = h - 12;
+    ampm = "PM";
+}
+
+h = h < 10 ? "0" + h : h; 
+m = m < 10 ? "0" + m : m;
+s = s < 10 ? "0" + s : s;
+
+hourEl.innerText = h;
+minutesEl.innerText = m;
+secondsEl.innerText = s;
+ampmEl.innerText = ampm;
+setTimeout(() =>{
+         clocktime();
+    }, 1000);
+}
+clocktime();
